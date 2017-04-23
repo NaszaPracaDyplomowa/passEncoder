ac33e8544e2357f4ed3930bd90d016f57493a46d
nasza.praca.dyplomowa <nasza.praca.dyplomowa@gmail.com>
2017-04-23 12:15:07 +0200

    fix

--- src/passEncoder/Main.java
+++ src/passEncoder/Main.java
@@ -4,12 +4,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Main {
     public static void main(String[] args) {
-    	if(args.length == 1){
-            String password = args[0];
-            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            System.out.println(passwordEncoder.encode(password));
-    	} else {
-    		System.out.println("Args error " + args.length);
+    	try{
+    		if(args.length == 1){
+    			String password = args[0];
+    			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    			System.out.println(passwordEncoder.encode(password));
+    		} else {
+    			System.out.println("Error " + args.length);
+    		}
+    	} catch(Exception ex) {
+    		ex.printStackTrace();  		
     	}
     }
 }
